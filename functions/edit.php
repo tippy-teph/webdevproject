081d1bc259575941537c01f936fd34630d8d1012
tippy-teph <143627067+tippy-teph@users.noreply.github.com>
2023-11-01 23:19:07 +0800

    Update edit.php

--- functions/edit.php
+++ functions/edit.php
@@ -1,21 +1,36 @@
 <?php
-    include_once("../dbConnection/.php");
-    if(isset($_POST['update'])) {
-        $id = $_POST['id'];
-        $id = $_POST['code'];
-        $id = $_POST['name'];
-
-        if(empty($code) || empty($name)) {
-            if(empty($code)) {
-                echo "<font color='red'>Subject Code field is empty.</font><br/>";
-            }
-            if(empty($name)) {
-                echo "<font color='red'>Subject Name field is empty.</font><br/>";
-            }
-        }
-        else{
-            mysql_query($dbc,"UPDATE tblsubject SET subject_code='$code', subject_name='$name' WHERE subject_id='$id'");
-            header("Location: ../index.php")
-        }
+    include_once("../dbConnection/mysqlconfig_connection.php");
+    $id = $_GET['id'];
+    $result = mysqli_query($dbc, "SELECT * FROM tblsubjects WHERE Subject_ID=$id");
+    while($res = mysqli_fetch_array($result)) {
+        $code = $res['Subject_Code'];
+        $name = $res['Subject_Name'];
     }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Edit Data</title>
+    </head>
+<body>
+    <h1>Edit Subject</h1>
+    <a href="../index.php">Home</a>
+    <br/><br/>
+    <form name="form1" method="post" action="../functions/edit.php">
+        <table boder="0">
+            <tr>
+                <td>Subject Code</td>
+                <td><input type="text" name="code" value="<?php echo $code;?>"></td>
+            </tr>
+            <tr>
+                <td>Subject Name</td>
+                <td><input type="text" name="name" value="<?php echo $name;?>"></td>
+            </tr>
+            <tr>
+                <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?></td>
+                <td><input type="submit" name="update" value="Update"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
