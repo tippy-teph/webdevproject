8318cb8ec20da9f16f6dff6d3d3f2d9da2269369
tippy-teph <143627067+tippy-teph@users.noreply.github.com>
2023-11-01 22:57:12 +0800

    Create add.php

---
+++ functions/add.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add Data</title>
+</head>
+    <body>
+        <?php
+            include_once("../dbConnection/mysqlconfig_connection.php");
+
+            if(isset($_POST['Submit'])){
+                $code = $_POST['code'];
+                $name = $POST_['name'];
+
+                if(empty($code) || empty($name)) {
+                    if(empty($code)){
+                        echo "<font color='red'>Subject Code field is empty.</font><br/>";
+                    }
+                    if(empty($name)){
+                        echo "<font color='red'>Subject Name field is empty.</font><br/>";
+                    }
+                    echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+                }
+                else {
+                    $result = mysql_query($dbc, "INSERT INTO tblsubject(subject_code, subject_name)VALUES('$code','$name')");
+                    echo "<font color='green'>Data added successfully.";
+                    echo "<br/><a herf='../index.php'>View Result</a>";
+                }
+            }
+        ?>
+    </body>
+</html>
