6c4c80463ac0747f34c584ea47eb971b4d6bf368
tippy-teph <143627067+tippy-teph@users.noreply.github.com>
2023-11-28 22:26:28 +0800

    Update fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,4 +1,14 @@
 <?php
-    $query = "SELECT * FROM tblsubject";
+    $query = "<<UPDATE SELECT QUERY>>";
     $result = mysql_query($dbc, $query);
+
+    function fetch($record_id){
+        $sql = "SELECT * FROM tblsubjects WHERE Subject_ID = :id;";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':id', $record_id);
+        if($query->execute()){
+            $data = $query->fetch();
+        }
+        return $data;
+    }
 ?>
