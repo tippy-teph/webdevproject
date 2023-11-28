9b38d76010375e693a7a0dfcac27f21525bd713a
tippy-teph <143627067+tippy-teph@users.noreply.github.com>
2023-11-28 23:39:06 +0800

    Create dbschedule.sql

---
+++ dbschedule.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2023 at 04:06 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbschedule`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubjects`
+--
+
+CREATE TABLE `tblsubjects` (
+  `Subject_ID` int(11) NOT NULL,
+  `Subject_Code` varchar(50) NOT NULL,
+  `Subject_Name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsyllabus`
+--
+
+CREATE TABLE `tblsyllabus` (
+  `syllabus_id` int(11) NOT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `syllabus_code` varchar(255) DEFAULT NULL,
+  `syllabus_author` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblsubjects`
+--
+ALTER TABLE `tblsubjects`
+  ADD PRIMARY KEY (`Subject_ID`);
+
+--
+-- Indexes for table `tblsyllabus`
+--
+ALTER TABLE `tblsyllabus`
+  ADD PRIMARY KEY (`syllabus_id`),
+  ADD KEY `subject_id` (`subject_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tblsyllabus`
+--
+ALTER TABLE `tblsyllabus`
+  ADD CONSTRAINT `tblsyllabus_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `tblsubjects` (`Subject_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
