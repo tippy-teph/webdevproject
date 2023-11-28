db870dc6676f2837b445d447b542dcb551590a75
tippy-teph <143627067+tippy-teph@users.noreply.github.com>
2023-11-28 22:27:17 +0800

    Update delete.php

--- functions/delete.php
+++ functions/delete.php
@@ -1,6 +1,22 @@
 <?php
     include_once("../dbConnection/mysqlconfig_connection.php");
     $id = $_GET['id'];
-    mysqli_query($dbc, "DELETE FROM tblsubject WHERE subject_id='$id'");
+    mysqli_query($dbc, "DELETE FROM tblsubjects WHERE Subject_ID='$id'");
     header("Location:../index.php");
+
+    function delete(){
+        $sql = "DELETE from tblsubjects WHERE Subject_ID = :id;";
+
+        $query=$this->db->connect()->prepare($sql);
+        $$query->bindParam(':Subject_Code', $this->code);
+        $query->bindParam(':Subject_Name', $this->name);
+        
+        if($query->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }	
+    }
 ?>
+
