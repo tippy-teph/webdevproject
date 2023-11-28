aa9f425d036c5a7a8d412cea81a16f4335573132
tippy-teph <143627067+tippy-teph@users.noreply.github.com>
2023-11-28 22:29:18 +0800

    Update editform.php

--- forms/editform.php
+++ forms/editform.php
@@ -1,10 +1,10 @@
 <?php
     include_once("../dbConnection/mysqlconfig_connection.php");
     $id = $_GET['id'];
-    $result = mysqli_query($dbc, "SELECT * FROM tblsubject WHERE subject_id=$id");
+    $result = mysqli_query($dbc, "SELECT * FROM tblsubjects WHERE Subject_ID=$id");
     while($res = mysqli_fetch_array($result)) {
-        $code = $res['Subject_code'];
-        $name = $res['Subject_name'];
+        $code = $res['Subject_Code'];
+        $name = $res['Subject_Name'];
     }
 ?>
 <!DOCTYPE html>
